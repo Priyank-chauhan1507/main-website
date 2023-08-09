c9b836af3969b394faae92bb72638714a93d16d2
Tarun Kansal <127049885+BhawnaKansal@users.noreply.github.com>
2023-08-10 01:13:23 +0530

    RegComplete and emailVerified page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
-
+import EmailVerify from "./components/Registration/College_Details/EmailVerify/EmailVerify";
+import RegisComp from "./components/Registration/College_Details/RegComp/RegCompleted";
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/email" element={<EmailVerify/>} />
+          <Route path="/regComp" element={<RegisComp/>} />
         </Routes>
       </Router>
     </>
