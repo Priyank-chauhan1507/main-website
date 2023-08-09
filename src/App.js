4d41bda6df64ce826300072caf989e2c099e70e1
Dewansh Upadhyay <dewansh.upadhyay2017@gmail.com>
2023-08-10 03:13:47 +0530

    Add Personal Details css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 
+// import CollegeDetails from "./components/registration/CollegeDetailes/Collegedetailes";
+// import RegPage from "./components/registration/RegistrationPage/RegPage";
+import PersonalDetails from "./components/registration/step1/personalDetails";
+
+// import PersonalDetails from "./components/registration/step1/personalDetails";
+
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-        </Routes>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+          {/* <Route exact={true} path="/registration" element={<RegPage />} />
+          <Route exact={true} path="/collegedetailes" element={<CollegeDetails />} /> */}
+          <Route exact={true} path="/personaldetails" element={<PersonalDetails />} />
+      </Routes>
       </Router>
     </>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,14 +5,16 @@ import singers from "../../assests/singers.png"
 import singersmob from "../../assests/singersmob.png"
 import "./Home.scss"
 import Navbar from '../Navbar/Navbar'
+import {useNavigate} from "react-router-dom"
 function Home() {
+  const navigate=useNavigate();
   return (
     <div className='home'>
         <img src={Homebg}  className='homebg'
         alt="" />
         <Navbar/>
         <img src={thomso} alt="" className='thomso'/>
-        <button className='register'>Register Now!</button>
+        <button className='register' onClick={()=>navigate("/registration")}>Register Now!</button>
         <img src={singers} alt="" className='singers'/>
         <img src={singersmob} alt="" className='singersmob'/>
     </div>
