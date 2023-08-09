6d0e9fdd177ea968ad2f92cf366b67871711b3a2
Aryan-singhal <aryan.singhal613@gmail.com>
2023-08-10 02:25:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Login from "./components/Registration/login/Login";
+
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route exact={true} path="/" element={<Home/>}/>
+          <Route exact={true} path="/login" element={<Login/>}/>
         </Routes>
       </Router>
     </>
