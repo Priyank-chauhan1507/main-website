ec0b1df4a85b24142764fc1eda9d3e338c73948a
SidxRengen <gautamsiddharth226@gmail.com>
2023-08-09 22:04:07 +0530

    home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Homebg from "../../assests/Landing-page.webp"
+import thomso from "../../assests/thomso.png"
+import singers from "../../assests/singers.png"
+import singersmob from "../../assests/singersmob.png"
+import "./Home.scss"
+import Navbar from '../Navbar/Navbar'
+function Home() {
+  return (
+    <div className='home'>
+        <img src={Homebg}  className='homebg'
+        alt="" />
+        <Navbar/>
+        <img src={thomso} alt="" className='thomso'/>
+        <button className='register'>Register Now!</button>
+        <img src={singers} alt="" className='singers'/>
+        <img src={singersmob} alt="" className='singersmob'/>
+    </div>
+  )
+}
+
+export default Home
