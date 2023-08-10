2a6512f10cf2ac0c9d95879457e7e9a390d1c62e
Dewansh Upadhyay <dewansh.upadhyay2017@gmail.com>
2023-08-11 00:05:22 +0530

    forgot password mainreg

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
-import Login from "./components/Registration/login/Login";
-import PersonalDetails from "./components/Registration/step2/personalDetails";
-import EmailVerify from "./components/Registration/EmailVerify/EmailVerify";
-import RegisComp from "./components/Registration/RegComp/RegCompleted";
+import Login from "./components/registration/login/Login";
+
+import PersonalDetails from "./components/registration/step2/personalDetails";
+import EmailVerify from "./components/registration/EmailVerify/EmailVerify";
+import RegisComp from "./components/registration/RegComp/RegCompleted";
+
+// import CollegeDetails from "./components/registration/CollegeDetailes/Collegedetailes";
+// import RegPage from "./components/registration/RegistrationPage/RegPage";
+// import PersonalDetails from "./components/registration/step1/personalDetails";
+
+// import PersonalDetails from "./components/registration/step1/personalDetails";
+
+
+// import EmailVerify from "./components/Registration/College_Details/EmailVerify/EmailVerify";
+// import RegisComp from "./components/Registration/College_Details/RegComp/RegCompleted";
+
+import Forget from "./components/registration/forget_pwd/forget";
+
 function App() {
   return (
     <>
@@ -16,6 +30,10 @@ function App() {
           <Route path="/emailverified" element={<EmailVerify/>} />
           <Route path="/regcompleted" element={<RegisComp/>} />
           <Route exact={true} path="/login" element={<Login/>}/>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/email" element={<EmailVerify/>} />
+          <Route path="/regComp" element={<RegisComp/>} />
+          <Route exact={true} path="/forget" element={<Forget/>} />
         </Routes>
       </Router>
     </>
