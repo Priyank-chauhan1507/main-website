928205cb0d6d5579d486d1c10c41329d810bce3e
KaranBansalwins <140838408+KaranBansalwins@users.noreply.github.com>
2023-08-10 23:36:14 +0530

    Profilenavbar

---
+++ src/components/Navbar/Profilenavbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./ProfilenavbarStyle.css"
+import logo from "../../assests/logo.svg";
+import lastimg from "../../assests/lastimg.png";
+import Medianav0 from './Medianav0';
+export default function Profilenavbar() {
+  return (
+    <div>
+      <div className="navbar-container-0">
+        <div className="navbar-img-0"><img src={logo} alt="" /></div>
+        <div className="navbar-container-02">
+          <div className="navbar-list-0">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/">Why visit Thomso?</a></li>
+              <li><a href="/">Zonals</a></li>
+              <li><a href="/">MUN</a></li>
+              <li><a href="/">Sponsors</a></li>
+              <li><a href="/">Contact Us</a></li>
+            </ul></div>
+          <div className="navbar-end-img-0"><img src={lastimg} alt="" /></div>
+        </div>
+      </div>
+      <Medianav0 />
+      
+    </div>
+  )
+}
