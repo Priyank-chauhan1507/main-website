4f5c5f6241fcfc92e3fd093383c291b53758005e
SidxRengen <gautamsiddharth226@gmail.com>
2023-08-13 04:19:53 +0530

    changes

--- src/components/Profile/Profile/Profile.js
+++ src/components/Profile/Profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Profile.css";
-import LeftSideProfile from "../LeftSideProfile/leftsideprofile";
+import LeftSideProfile from "../LeftSideProfile/Leftsideprofile";
 import MainProfileBox from "../MainProfileBox/MainProfileBox";
 import NewNewProfileMobile from "../MobProfile/NewNewProfileMobile";
 // import EventMobileNav from "../EventsPage/EventMobileNav";
