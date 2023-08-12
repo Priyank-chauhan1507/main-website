38bb36355f2dcabd8d8297dacf54b4d14f08e78a
RamChhabra21 <101047917+RamChhabra21@users.noreply.github.com>
2023-08-12 06:46:55 +0530

    navbar changes

--- src/components/Navbar/Medianav0.js
+++ src/components/Navbar/Medianav0.js
@@ -1,19 +1,74 @@
-import React from 'react'
+import React , { useState } from 'react'
 import "./MedianavStyle.css"
 import logo from "../../assests/logo.svg";
+import Cross from "../../assests/Cross.webp";
+import { Link, useNavigate } from "react-router-dom";
+import imgnav from "../../assests/imgnav.webp";
+import logo1 from "../../assests/Logo23.svg";
+import { RxCross2 } from "react-icons/rx";
+
 export default function Medianav0() {
+  const [open, setOpen] = useState(true);
+  const [isActive, setisActive] = useState(false);
+  const handleClick1 = () => {
+    setisActive(!isActive);
+  };
+  const generalDetails = [
+    {
+      name: "Home",
+      to: "/",
+    },
+    {
+      name: "Why visit Thomso? ",
+      to: "/",
+    },
+    {
+      name: "Zonals",
+      to: "https://zonals.thomso.in",
+    },
+    {
+      name: "MUN",
+      to: "/newprofile",
+    },
+    {
+      name: "Spons",
+      // to: "/newprofile",
+      to: "/",
+      target:"_blank"
+      
+    },
+    {
+      name: "Contact Us",
+      to: "/",
+    },
+  ];
   return (
     <div>
           <div className="hamburg">
         <div className="hamburg-logo"><img src={logo} alt="" /> </div>
         <div className="hamburg-style">
-            <div className="spanlist">
+            <div className="spanlist" onClick={handleClick1}> 
             <span></span>
             <span></span>
             <span></span>
             </div>
         </div>
       </div>
+      <div className={`allData ${isActive ? "leftPos" : "notLeftPos"}`}>
+        <div className="navMainImg" onClick={handleClick1}>
+          <p id="cross">
+            <RxCross2 size={28} />
+          </p>
+        </div>
+      
+        {generalDetails.map((post, idx) => {
+          return (
+            <div key={idx} className="generalDetails">
+              <Link to={post.to} target={post.target}>{post.name} </Link>
+            </div>
+          );
+        })}
+          </div>
     </div>
   )
 }
