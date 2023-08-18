d19c4847a6d891576481fbe5c9af57745e7fc33c
Rishabh Gupta <rg074523@gmail.com>
2023-08-19 04:14:33 +0530

    changes

--- src/components/EventsNavbar/Eventsnavbar.js
+++ src/components/EventsNavbar/Eventsnavbar.js
@@ -177,8 +177,22 @@ function Navbar({ color, disable, setregister, register }) {
                     >
                       Mr & Ms Thomso
                     </div>
-                    <div className="col colhover">Vogue</div>
-                    <div className="col colhover">Cosplay</div>
+                    <div
+                      className="col colhover"
+                      onClick={(e) => {
+                        onHandleClick(78);
+                      }}
+                    >
+                      Vogue
+                    </div>
+                    <div
+                      className="col colhover"
+                      onClick={(e) => {
+                        onHandleClick(63);
+                      }}
+                    >
+                      Coscon
+                    </div>
                     <div className="col col-yellow">LITFest</div>
                     <div
                       className="col colhover"
@@ -223,7 +237,7 @@ function Navbar({ color, disable, setregister, register }) {
                     <div
                       className="col colhover"
                       onClick={(e) => {
-                        onHandleClick(28);
+                        onHandleClick(30);
                       }}
                     >
                       Slam Poetry
@@ -231,10 +245,10 @@ function Navbar({ color, disable, setregister, register }) {
                     <div
                       className="col colhover"
                       onClick={(e) => {
-                        onHandleClick(30);
+                        onHandleClick(28);
                       }}
                     >
-                      Nerdy Bait
+                      Nerdy Bate
                     </div>
                   </div>
                   <div className="drop-row">
@@ -433,7 +447,14 @@ function Navbar({ color, disable, setregister, register }) {
                       Abhivyakti
                     </div>
 
-                    <div className="col colhover">Nukkad Natak</div>
+                    <div
+                      className="col colhover"
+                      onClick={(e) => {
+                        onHandleClick(79);
+                      }}
+                    >
+                      Nukkad Natak
+                    </div>
                     {/* <div className="col colhover">Mimicry</div>
                     <div className="col colhover">Monologue</div> */}
 
@@ -558,8 +579,22 @@ function Navbar({ color, disable, setregister, register }) {
                   </div>
                   <div className="drop-row">
                     <div className="col col-yellow">Online</div>
-                    <div className="col colhover">Bgmi</div>
-                    <div className="col colhover">Quizardry</div>
+                    <div
+                      className="col colhover"
+                      onClick={(e) => {
+                        onHandleClick(80);
+                      }}
+                    >
+                      Bgmi
+                    </div>
+                    <div
+                      className="col colhover"
+                      onClick={(e) => {
+                        onHandleClick(81);
+                      }}
+                    >
+                      Quizardry
+                    </div>
                     <div className="col col-yellow">Night Life</div>
                     <div
                       className="col colhover"
@@ -569,12 +604,47 @@ function Navbar({ color, disable, setregister, register }) {
                     >
                       Silent DJ
                     </div>
-                    <div className="col colhover">Nightlife Cafe</div>
-                    <div className="col colhover ">Karaoke</div>
+                    <div
+                      className="col colhover"
+                      onClick={(e) => {
+                        onHandleClick(82);
+                      }}
+                    >
+                      Nightlife Cafe
+                    </div>
+                    <div
+                      className="col colhover "
+                      onClick={(e) => {
+                        onHandleClick(83);
+                      }}
+                    >
+                      Karaoke
+                    </div>
                     <div className="col col-yellow">New Events</div>
-                    <div className="col colhover">Sneaker Design*</div>
-                    <div className="col colhover">Mimicry*</div>
-                    <div className="col colhover">Monologue*</div>
+                    <div
+                      className="col colhover"
+                      onClick={(e) => {
+                        onHandleClick(84);
+                      }}
+                    >
+                      Sneaker Design*
+                    </div>
+                    <div
+                      className="col colhover"
+                      onClick={(e) => {
+                        onHandleClick(85);
+                      }}
+                    >
+                      Mimicry*
+                    </div>
+                    <div
+                      className="col colhover"
+                      onClick={(e) => {
+                        onHandleClick(86);
+                      }}
+                    >
+                      Monologue*
+                    </div>
                     <div
                       className="col colhover"
                       onClick={(e) => {
@@ -583,7 +653,14 @@ function Navbar({ color, disable, setregister, register }) {
                     >
                       Thomsography*
                     </div>
-                    <div className="col colhover">Vlogging*</div>
+                    <div
+                      className="col colhover"
+                      onClick={(e) => {
+                        onHandleClick(87);
+                      }}
+                    >
+                      Vlogging*
+                    </div>
                   </div>
                 </div>
               </>
