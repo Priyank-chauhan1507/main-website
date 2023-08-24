71b4aa0c15df730a47416c8ab19655694a9589ef
Rishabh Gupta <rg074523@gmail.com>
2023-08-24 13:39:31 +0530

    changes

--- src/components/Navbar/WebNavbarNew.js
+++ src/components/Navbar/WebNavbarNew.js
@@ -16,13 +16,13 @@ const WebNavbarNew = () => {
       <div className="webNavbarMain">
         <div className="webNavbarLeft">
           <div className="webNavbarLeft1">Contact us</div>
-          <div className="webNavbarLeft2">
-            <div>Kartikey</div>
-            <div>9205915692</div>
-          </div>
           <div className="webNavbarLeft3">
             <div>Jolly</div>
             <div> 7081537653</div>
+          </div>
+          <div className="webNavbarLeft2">
+            <div>Kartikey</div>
+            <div>9205915692</div>
           </div>
         </div>
         <div className="webNavbarMiddle">
