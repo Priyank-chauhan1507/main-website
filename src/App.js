1261952c40060931f9c64d1c551ad7e6407094dd
Its-uks <utkarshv1213@gmail.com>
2023-09-01 23:46:57 +0530

    create mun folder

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ import Idcard from "./components/Profile/ID_card/Idcard";
 import IdcardBox from "./components/Profile/ID_card/idcardBox";
 import EventsMain from "./components/Profile/EventsPage/EventMainPage"
 import ProfileNameEditModel from "./components/Profile/MainProfileBox/ProfileNameEditModel";
-import Home from "./components/Home/Home";
+import Home1 from "./components/Home/Home";
 import ReactGA from "react-ga4";
 // require('dotenv').config()
 
@@ -78,7 +78,7 @@ function App(props) {
           <Route exact={true} path="/paymentsucess" element={<PaymentSuccess />} />
           <Route exact={true} path="/eventsmain" element={<EventsMain />} />
           <Route exact={true} path="/ProfileNameEdit" element={<ProfileNameEditModel />} />
-          <Route exact={true} path="/MUN" element={<Home />} />
+          <Route exact={true} path="/MUN" element={<Home1/>} />
         </Routes>
       </Router>
     </>
