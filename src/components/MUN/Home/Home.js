62c46b4989b8368765a5733733b1892fed00901a
Rishabh Gupta <rg074523@gmail.com>
2023-09-03 23:28:08 +0530

    changes

--- src/components/MUN/Home/Home.js
+++ src/components/MUN/Home/Home.js
@@ -6,13 +6,13 @@ import Navbar from "../../../components/EventsNavbar/Eventsnavbar";
 import Judges from '../Judges/Judges'
 import FAQ from '../FAQ/FAQ'
 import Footer from '../Footer/Footer'
-
+import Prizes from "../../../assests/PrizesWorth.png";
 
 function Home() {
   return (
     <div className="MUN-bg">
       <img src={BG} className="MUN-Bg" alt="" />
-      <Navbar/>
+      <Navbar color="transparent"/>
         <div className="top-MUN-img">
           <img src={MUNTop} alt="" />
         </div>
@@ -20,6 +20,12 @@ function Home() {
           <button className="registerNowBtnClick">
             Registser Now
           </button>
+        </div>
+        <div className="prizesWorth">
+          <div className="prizesWorthDiv">
+          <img src={Prizes} alt="" className="prizesWorth1" />
+          </div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non sed quisque auctor suspendisse tortor, risus. Natoque aliquet phasellus orci et a ut justo quis. Consequat arcu aliquam consectetur adipiscing cras ultricies ornare nec. Vulputate venenatis, blandit blandit
         </div>
         <Judges />
         <FAQ />
