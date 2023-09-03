997cf4fc0da25808ad406b8b5c26deeb48e52d1e
RamChhabra21 <101047917+RamChhabra21@users.noreply.github.com>
2023-09-03 14:15:27 +0530

    mun_faq

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import EventsMain from "./components/Profile/EventsPage/EventMainPage"
 import ProfileNameEditModel from "./components/Profile/MainProfileBox/ProfileNameEditModel";
 // import Home1 from "./components/Home/Home";
 import Home1 from "./components/MUN/Home/Home";
+import FAQ from "./components/MUN/FAQ/FAQ";
 import Footer1 from "./components/MUN/Footer/Footer";
 // import Footer1 from "./components/MUN/Judges/Judges";
 import ReactGA from "react-ga4";
@@ -87,6 +88,7 @@ function App(props) {
           <Route exact={true} path="/eventsmain" element={<EventsMain />} />
           <Route exact={true} path="/ProfileNameEdit" element={<ProfileNameEditModel />} />
           <Route exact={true} path="/MUN" element={<Home1/>} />
+          <Route exact={true} path="/MUN_FAQ" element={<FAQ/>} />
           <Route exact={true} path="/munfooter" element={<Footer1/>} />
           <Route exact={true} path="/judges" element={<Judges/>} />
           <Route exact={true} path="/merch_page1" element={<Page1/>} />
