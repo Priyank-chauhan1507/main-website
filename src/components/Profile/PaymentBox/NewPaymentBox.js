fcdd73e657412e8044ef80a93067236be404e865
Rishabh Gupta <rg074523@gmail.com>
2023-10-12 00:47:00 +0530

    change

--- src/components/Profile/PaymentBox/NewPaymentBox.js
+++ src/components/Profile/PaymentBox/NewPaymentBox.js
@@ -1026,9 +1026,9 @@ const NewPaymentBox = (
                     <div className="lsp-c1">
                       <img src={icon3} alt="icon3" />
                       <Link
-                        to="/payment_gupt"
+                        to="/payment_gupta"
                         className={
-                          locator.pathname === "/payment_gupt"
+                          locator.pathname === "/payment_gupta"
                             ? "c1-text"
                             : "c2-text"
                         }
@@ -1441,9 +1441,9 @@ const NewPaymentBox = (
                   ) : (
                     <div className="mv-top-2">
                       <Link
-                        to="/payment_gupt"
+                        to="/payment_gupta"
                         className={
-                          Locator.pathname === "/payment_gupt"
+                          Locator.pathname === "/payment_gupta"
                             ? "nav-active"
                             : "nav-passive"
                         }
